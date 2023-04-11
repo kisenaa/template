a4d3ab610bb45ad0de73462a1482774928b64e04
Johannes <johannesdanis304@gmail.com>
2023-04-11 14:57:08 +0700

    fix: merge git issue

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,7 @@ module.exports = {
   "*.{ts,tsx,css}": ["prettier . --write"],
   "*.{ts,tsx}": [
     "eslint . --cache --fix --ext .tsx --ext .ts",
+    () => "pnpm tsc",
+    () => "pnpm jest",
   ],
 };
